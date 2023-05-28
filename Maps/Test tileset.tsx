0804be2c613954463074a1d3b0fd84eb938938f0
GE <91962195+Gregor-Edwards@users.noreply.github.com>
2023-05-29 00:07:59 +0100

    Delete Maps directory

--- Maps/Test tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Test tileset" tilewidth="16" tileheight="16" tilecount="324" columns="18">
- <image source="C:/Users/gregor/Downloads/set.gif" trans="000000" width="288" height="288"/>
-</tileset>
